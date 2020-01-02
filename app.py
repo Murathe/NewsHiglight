7e50cdfd42d4ce0a8ed0ff8c431cdf14475b1947
murathe <isaacsm07p@gmail.com>
2020-01-02 16:27:29 +0300

    Return results

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
-    
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    poster = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles[])
