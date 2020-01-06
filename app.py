4f4c7037401dfc6b948c46e5bc2a30ea76ae21ae
murathe <isaacsm07p@gmail.com>
2020-01-06 13:59:51 +0300

    Argaam def func

--- app.py
+++ app.py
@@ -93,6 +93,26 @@ def Fox():
     mylist = zip(news, desc, img)
     return render_template('fox.html', context = mylist)
 
+@app.route('/argaam')
+def Argaam():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="argaam")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+    return render_template('fox.html', context = mylist)
 
 
 if __name__ == "__main__":
