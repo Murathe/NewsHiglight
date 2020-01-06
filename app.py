df28f8c7b50e941a319c58b8ca3d6471515db175
murathe <isaacsm07p@gmail.com>
2020-01-06 11:23:59 +0300

    Def ansa new source

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def Bbc():
     news = []
     desc = []
     img = []
-    
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -50,6 +49,28 @@ def Bbc():
     mylist = zip(news, desc, img)
 
     return render_template('bbc.html', context = mylist)
+
+@app.route('/ansa')
+def Ansa():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+
+    return render_template('ansa.html', context = mylist)
 
 if __name__ == "__main__":
     app.run(debug = True)
