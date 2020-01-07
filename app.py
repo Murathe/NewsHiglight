c850808079ab845333d6df3b7312eadd17de9bc4
murathe <isaacsm07p@gmail.com>
2020-01-07 09:54:14 +0300

    deplyoment to heroku

--- app.py
+++ app.py
@@ -28,6 +28,82 @@ def Index():
 
     return render_template('index.html')
 
+@app.route('/ars')
+def Ars():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="ars-technica")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    pubAt = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img, pubAt, url)
+
+    return render_template('ars.html', context = mylist)
+
+@app.route('/google')
+def Google():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="google-news")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    pubAt = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img, pubAt, url)
+    return render_template('google.html', context = mylist)
+
+@app.route('/verge')
+def Theverge():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="the-verge")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    pubAt = []
+    url = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img, pubAt, url)
+    return render_template('verge.html', context = mylist)
+
 @app.route('/bbc')
 def Bbc():
     newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
@@ -38,6 +114,8 @@ def Bbc():
     news = []
     desc = []
     img = []
+    pubAt = []
+    url = []
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -45,32 +123,12 @@ def Bbc():
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img, pubAt, url)
 
     return render_template('bbc.html', context = mylist)
-
-@app.route('/ansa')
-def Ansa():
-    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
-    topheadlines = newsapi.get_top_headlines(sources="ansa-news")
-
-    articles = topheadlines['articles']
-
-    news = []
-    desc = []
-    img = []
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-
-    mylist = zip(news, desc, img)
-
-    return render_template('ansa.html', context = mylist)
 
 @app.route('/fox')
 def Fox():
@@ -82,6 +140,8 @@ def Fox():
     news = []
     desc = []
     img = []
+    pubAt = []
+    url = []
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -89,31 +149,11 @@ def Fox():
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img, pubAt, url)
     return render_template('fox.html', context = mylist)
-
-@app.route('/argaam')
-def Argaam():
-    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
-    topheadlines = newsapi.get_top_headlines(sources="argaam")
-
-    articles = topheadlines['articles']
-
-    news = []
-    desc = []
-    img = []
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-
-    mylist = zip(news, desc, img)
-    return render_template('fox.html', context = mylist)
-
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY = c40d6d5c48504741980b4e27910dd4a4
+export secret_key = fl@3ksi0game
+
+python3.6 manage.py server
